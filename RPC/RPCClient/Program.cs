efe4c27ef7052c345e266d348c5d1cada619ad0e
wang2245 <wang2245@gmail.com>
2020-07-27 17:24:11 +0800

    add demo

---
+++ RPC/RPCClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
